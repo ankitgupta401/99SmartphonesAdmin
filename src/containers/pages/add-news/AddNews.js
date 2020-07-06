1b421c448130ed9bb9eba56da75a43d9f55f0f58
Ankit Gupta <ag3831124@gmail.com>
2020-07-06 17:03:14 +0530

    news integration complete

--- src/containers/pages/add-news/AddNews.js
+++ src/containers/pages/add-news/AddNews.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import { Card, Input, TextField } from '@material-ui/core';
+import { Card, TextField, Input } from '@material-ui/core';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-
+import { css } from "@emotion/core";
+import CircleLoader from "react-spinners/CircleLoader";
+import axios from '../../../axios-url';
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 let paras = [];
 const AddNews = () => {
     const [state, setState] = useState({
@@ -16,40 +22,77 @@ const AddNews = () => {
             paras: [],
 
         },
-        save:false
+        save: false,
+        loading: false
 
     });
 
     const contentChange = (e, place) => {
-        let temp = { ...state };
-        temp.data = { ...temp.data };
-        temp.data[place] = e;
-        setState(() =>{return temp});
+       
+        setState((prevState) => { 
+            let temp = { ...prevState };
+            temp.data = { ...prevState.data };
+            temp.data[place] = e;
+            return temp 
+        });
     }
 
     const onImageChange = (e, place) => {
-        console.log(e, place);
+        setState((prevState) => { return { ...prevState, loading: true } });
+        console.log(e.target.files[0], place);
+        let formData = new FormData();
+        formData.append('file', e.target.files[0]);
 
+        axios({
+            method: 'post',
+            url: 'upload_news_image',
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        }).then(res => {
+            
+            setState(prevState => {
+                let temp = { ...prevState };
+                temp.data = {...prevState.data};
+                temp.data[place] = res.data.result;
+                temp.loading = false;
+                return temp;
+            });
+        });
     }
 
     const paragraphDataChange = (data, place, index) => {
 
         setState(prevState => {
-            
-        let temp = { ...prevState };
-        temp.data = { ...prevState.data };
-        temp.data.paras = [...prevState.data.paras];
-        temp.data.paras[index][place] = data;
-            return temp 
-        },      console.log(state));
-  
+
+            let temp = { ...prevState };
+            temp.data = { ...prevState.data };
+            temp.data.paras = [...prevState.data.paras];
+            temp.data.paras[index][place] = data;
+            return temp
+        }, console.log(state));
+
     }
-    const paragraphImageChange = (file, place, index) => {
-        let temp = { ...state };
-        temp.data = { ...state.data };
-        temp.data.paras = [...state.data.paras];
-        temp.data.paras[index][place] = 'img link';
-        setState(prevState => { return { ...prevState, ...temp } });
+    const paragraphImageChange = (e, place, index) => {
+        setState((prevState) => { return { ...prevState, loading: true } });
+        console.log(e.target.files[0], place);
+        let formData = new FormData();
+        formData.append('file', e.target.files[0]);
+        axios({
+            method: 'post',
+            url: 'upload_news_image',
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        }).then(res => {
+            setState(prevState => {
+                let temp = { ...prevState };
+                temp.data = { ...prevState.data };
+                temp.data.paras = [...prevState.data.paras];
+                temp.data.paras[index][place] = res.data.result;
+                temp.loading = false;
+                return temp;
+            })
+        });
+
     }
     const paraChanged = (e) => {
         console.log(e);
@@ -58,7 +101,7 @@ const AddNews = () => {
         }
         let length = paras.length > e ? paras.length - e : e - paras.length;
         let addOrSub = paras.length > e ? 1 : 0; // 1 =sub , 0 = add
-        console.log(state , 'length', length, 'addor sub' , addOrSub , paras);
+        console.log(state, 'length', length, 'addor sub', addOrSub, paras);
         for (let i = 0; i < length; i++) {
             if (addOrSub === 0) {
                 paras.push(
@@ -80,25 +123,25 @@ const AddNews = () => {
                                 {/* <TextareaAutosize aria-label="minimum height" rowsMin={4} cols={60} placeholder="Enter The Title" /> */}
                                 <CKEditor
                                     editor={ClassicEditor}
-                                    data= {"<p>Enter The Content Here " +  i +"</p>"} 
+                                    data=""
                                     onInit={editor => {
                                         // You can store the "editor" and use when it is needed.
                                         console.log('Editor is ready to use!', editor);
-                              
-                                      setState(prevState => {
-                                          let temp ={...prevState};
-                                          temp.data = {...prevState.data};
-                                          temp.data.paras =[...prevState.data.paras];
-                                          temp.data.paras.push({sub_title:'', content:editor.getData(), image:''})
-                                         
-                                           return temp;
-                                      })
-                                    
-                                    
+
+                                        setState(prevState => {
+                                            let temp = { ...prevState };
+                                            temp.data = { ...prevState.data };
+                                            temp.data.paras = [...prevState.data.paras];
+                                            temp.data.paras.push({ sub_title: '', content: editor.getData(), image: '' })
+
+                                            return temp;
+                                        })
+
+
                                     }}
                                     onChange={(event, editor) => {
                                         const data = editor.getData();
-                                       // console.log({ event, editor, data });
+                                        // console.log({ event, editor, data });
                                         paragraphDataChange(data, 'content', i);
                                     }}
                                     onBlur={(event, editor) => {
@@ -111,7 +154,7 @@ const AddNews = () => {
                             </div>
                             <div className="col-md-3 col-sm-12">
                                 <label>Image</label><br />
-                                <TextField type="file" onChange={(e) => paragraphImageChange(e.target.files, 'image', i)}></TextField>
+                                <TextField type="file" onChange={(e) => paragraphImageChange(e, 'image', i)}></TextField>
                             </div>
 
                         </div>
@@ -119,95 +162,101 @@ const AddNews = () => {
                 )
             } else {
                 paras.pop();
-                setState(prevState => { 
-                  
+                setState(prevState => {
+
                     let temp = { ...prevState };
                     temp.data = { ...prevState.data };
                     temp.data.paras = [...prevState.data.paras];
                     temp.data.paras.pop();
-                    return { ...temp } 
+                    return { ...temp }
                 });
             }
 
         }
-        setState(() => {return { ...state, noOfParas: e }});
+        setState(() => { return { ...state, noOfParas: e } });
     }
     useEffect(() => {
-     //paraChanged(3)
-       return () => {
-           paras =[];
-       }
+        //paraChanged(3)
+        return () => {
+            paras = [];
+        }
     }, [])
 
-useEffect(() => {
-if(state.save === true){
-    //save
-    console.log(state);
-    setState(() => {return {
-        noOfParas: 0,
-        data: {
-            title: '',
-            mainImage: '',
-            paras: [],
+    useEffect(() => {
+        if (state.save === true) {
+            setState((prevState) => { return { ...prevState, loading: true } });
+            //save
+            axios.post("save_news", state.data).then(res => {
+                setState((prevState) => { return { ...prevState, loading: false, save: false } });
+                //save
+            });
 
-        },
-        save:false
-
-    }})
-}
-},[state.save])
+        }
+    }, [state.save])
 
     return (
         <Card>
-            <div>
-                <br />
-                <h2 style={{ paddingLeft: "6%" }}>Enter The News</h2>
-                <br />
+            <div className="sweet-loading d-flex justify-content-center align-items-center">
+                <CircleLoader
+                    css={override}
+                    size={80}
+                    color={"#F89C04"}
+                    loading={state.loading}
+                />
             </div>
-            <div  >
-                <form style={{ paddingLeft: "10%" }} noValidate autoComplete="off">
-                    <div className="row">
-                        <div className="col-md-6 col-sm-12">
+            {!state.save ? <div>
 
-                            <label>Title</label>
-                            <br />
-                            <TextareaAutosize aria-label="minimum height" onChange={(e) => { contentChange(e.target.value, 'title') }} rowsMin={2} cols={50} placeholder="Enter The Title" />
-                        </div>
-
-                        <div className="col-md-3 col-sm-12">
-                            <label>Upload Main Image</label> <br />
-                            <Input type="file" onChange={(e) => onImageChange(e.trarget.files, 'mainImage')}></Input>
-                        </div>
-
-                        <div className="col-md-2 col-sm-12">
-
-                            <label>Enter No Of Paragraphs</label>
-                            <br />
-                            <TextField id="outlined-basic" placeholder="No Of Paragraphs" defaultValue="" onChange={(e) => paraChanged(e.target.value)} variant="outlined" />
-                        </div>
-
-
-                    </div>
-                    <div>
-                        <br />
-                        <h4>Enter The Main Content</h4>
-                        <br />
-                    </div>
-                    {paras.map((val, i) => {
-                        return (
-                            <span key={i}>
-                                {val}
-                            </span>
-
-                        )
-                    })}
-                    <div className="d-flex justify-content-center" >
-                        <button type="button" className="btn btn-secondary" onClick={() => {setState({...state, save:true})}}>Save</button>
-                        <button style={{ marginLeft: "1%" }} type="reset" className="btn btn-danger">Clear</button>
-                    </div>
+                <div>
                     <br />
-                </form>
-            </div>
+                    <h2 style={{ paddingLeft: "6%" }}>Enter The News</h2>
+                    <br />
+                </div>
+                <div  >
+                    <form style={{ paddingLeft: "10%" }} noValidate autoComplete="off">
+                        <div className="row">
+                            <div className="col-md-6 col-sm-12">
+
+                                <label>Title</label>
+                                <br />
+                                <TextareaAutosize aria-label="minimum height" onChange={(e) => { contentChange(e.target.value, 'title') }} rowsMin={2} cols={50} placeholder="Enter The Title" />
+                            </div>
+
+                            <div className="col-md-3 col-sm-12">
+                                <label>Upload Main Image</label> <br />
+                                <Input type="file" onChange={(e) => onImageChange(e, 'mainImage')}></Input>
+                            </div>
+
+                            <div className="col-md-2 col-sm-12">
+
+                                <label>Enter No Of Paragraphs</label>
+                                <br />
+                                <TextField id="outlined-basic" placeholder="No Of Paragraphs" defaultValue="" onChange={(e) => paraChanged(e.target.value)} variant="outlined" />
+                            </div>
+
+
+                        </div>
+                        <div>
+                            <br />
+                            <h4>Enter The Main Content</h4>
+                         
+                        </div>
+                        {paras.map((val, i) => {
+                            return (
+                                <span key={i}>
+                                    {val}
+                                </span>
+
+                            )
+                        })}
+                          <br />  <br />
+                        <div className="d-flex justify-content-center" >
+                            <button type="button" className="btn btn-secondary" onClick={() => { setState({ ...state, save: true }) }}>Save</button>
+                            <button style={{ marginLeft: "1%" }} type="reset" className="btn btn-danger">Clear</button>
+                        </div>
+                        <br />
+                    </form>
+                </div>
+            </div> : ''}
         </Card>
     )
 }
