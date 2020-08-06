50830a102a55e16c2e0db4c04d57d508e9f80222
Ankit Gupta <ag3831124@gmail.com>
2020-08-06 17:07:20 +0530

    some changes

--- src/axios-url.js
+++ src/axios-url.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://blog.99smartphones.in/api/common/'
-    baseURL:'http://localhost:3000/api/common/'
+    baseURL: 'https://blog.99smartphones.in/api/common/'
+    // baseURL:'http://localhost:3000/api/common/'
 });
 
 export default instance;
